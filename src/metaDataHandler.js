ae15b004d0c8287c6742398d8b877516ec3f0a67
VishSeran <vishwa.sovis@gmail.com>
2025-10-29 14:13:35 +0530

    s3 bucket initialized

--- src/metaDataHandler.js
+++ src/metaDataHandler.js
@@ -1,6 +1,16 @@
+import { S3 } from "@aws-sdk/client-s3";
+
+
+const s3 = new S3({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },  
+});
 
 const metaDataHandler = async (event) => {
-
+    
 }
 
 export default metaDataHandler;
