49770d7504e5981d1c1ae1d05a3fd34ee3b4331f
VishSeran <vishwa.sovis@gmail.com>
2025-10-30 16:45:26 +0530

    upadate Bloackchain.js

--- src/Blockchain.js
+++ src/Blockchain.js
@@ -3,3 +3,17 @@ import DeedABI from "./deedABI.json" assert { type: "json" };
 
 const provider = new ethers.providersJsonRpcProvider(process.env.RPC_URL);
 const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, DeedABI, provider);
+
+export default async function (tokenId){
+    try{
+
+    }catch(error){
+        console.error("Error fetching metadata:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "Internal Server Error"
+            }),
+        };
+    }
+}
